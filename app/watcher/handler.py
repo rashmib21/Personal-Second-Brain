4bbeb3558fd064a733d652fd4506e330805bdc60
rashmib21 <rashmibarethiya2130@gmail.com>
2026-07-20 15:38:17 +0530

    create function for new file created

--- app/watcher/handler.py
+++ app/watcher/handler.py
@@ -1,5 +1,12 @@
 #Responsible for receiving file events, printing/logging them, later creating event objects
-from watchdog.events import FileSystemEventHandler
-from watchdog.observers import Observer
+
+from watchdog.events import FileSystemEventHandler #react for file modify, create, delete
+from watchdog.observers import Observer #watch a folder continously
 import time
 
+class SecondBrainHandler(FileSystemEventHandler):
+	def on_created(self, event):
+		if not event.is_directory: 
+			print("A new file is created: {event.src_path}")
+
+			
