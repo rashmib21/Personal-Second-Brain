6a2cc7060dc792597921b5cc7be7bc847bd5c0f8
rashmib21 <rashmibarethiya2130@gmail.com>
2026-07-20 16:36:45 +0530

    Send message to Kafka broker(topic)

--- app/kafka_producer/producer.py
+++ app/kafka_producer/producer.py
@@ -1,7 +1,33 @@
 from kafka import KafkaProducer
 import json
 
-def serializer(v):
+def value_serializer(v):
 	return json.dumps(v).encode('utf-8')
 
-	
+def key_serializer(v):
+	return json.dumps(v).encode('utf-8')
+
+
+producer=KafkaProducer(
+	bootstrap_servers=KAFKA_BOOTSTRAP_SERVERS,
+	value_serializer=value_serializer,
+	key_serializer=key_serializer
+	)	
+
+def publish_file_event(event_type, path):
+	#create event data
+	event={
+	"event":event_type,
+	"path":path,
+	"timestamp":time.time()
+	}
+
+	#Send event to kafka producer
+	producer.send(
+		"file-events", #kafka topic
+		key=path,
+		value=event
+		)
+
+	#check message is send
+	producer.flush()
