3fd542f6a79dcb729c511f9ca5958abde9366ff9
rashmib21 <rashmibarethiya2130@gmail.com>
2026-07-20 15:50:08 +0530

    create observer to keep watching the folder

--- app/watcher/handler.py
+++ app/watcher/handler.py
@@ -3,10 +3,35 @@
 from watchdog.events import FileSystemEventHandler #react for file modify, create, delete
 from watchdog.observers import Observer #watch a folder continously
 import time
+from config.py import *
 
 class SecondBrainHandler(FileSystemEventHandler):
 	def on_created(self, event):
 		if not event.is_directory: 
-			print("A new file is created: {event.src_path}")
+			print(f"A new file is created: {event.src_path}")
 
-			
+	def on_modified(self, event):
+		if not event.is_directory:
+			print(f"Modified: {event.src_path}")
+
+observer=Observer() #security guard, watching the folder
+
+handler=SecondBrainHandler() #handler reacting on that event
+
+#observer watch this folder and something happens, call the handler
+observer.schedule(handler, "WATCHED_FOLDER")
+
+observer.start() #start watching
+
+try:
+	while True:
+	time.sleep(1):
+
+except KeyboardInterrupt:
+	observer.stop()
+
+observer.join()	#wait here until the observer has completely stopped
+
+
+
+
