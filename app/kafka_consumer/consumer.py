e6dad2a1080189b6e1ca3a7df9d1d37151ff1bca
rashmib21 <rashmibarethiya2130@gmail.com>
2026-07-20 16:44:01 +0530

    initialize deserializer function and create consumer

--- app/kafka_consumer/consumer.py
+++ app/kafka_consumer/consumer.py
@@ -1,3 +1,14 @@
 from kafka import KafkaConsumer
 import json
+from config import KAFKA_BOOTSTRAP_SERVERS
 
+def value_deserializer(v):
+	return json.loads(v).decode('utf-8')
+
+consumer=KafkaConsumer(
+	'file-events'
+	bootstrap_servers=KAFKA_BOOTSTRAP_SERVERS,
+	value_deserializer=value_deserializer,
+	group_id='second-brain-workers',
+	auto_offset_reset='earliest'
+	)	
