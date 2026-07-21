4a1751a7305eca19e78e196b8ce2bb74169d5120
rashmib21 <rashmibarethiya2130@gmail.com>
2026-07-21 16:34:47 +0530

    initialize app of celery and redis broker, backend and enable task failure and worker failure condition

---
+++ app/celery_app/celery.py
@@ -0,0 +1,11 @@
+from celery import Celery
+from config import REDIS_HOST, REDIS_PORT
+
+app=Celery(
+	"second-brain",
+	broker=f"redis://{REDIS_HOST}:{REDIS_PORT}/0",
+	backend=f"redis://{REDIS_HOST}:{REDIS_PORT}/1",
+	)
+
+app.conf.task_acks_late=True #the broker marks as Done after the tasks finishes
+app.conf.task_reject_on_worker_lost=True  #if worker fails tasks goes back to the redis queue
