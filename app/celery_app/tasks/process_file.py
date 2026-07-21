bd601062b0de38fb5f1d54587a31498669801d3c
rashmib21 <rashmibarethiya2130@gmail.com>
2026-07-21 16:52:59 +0530

    define task along with retried and max retries

--- app/celery_app/tasks/process_file.py
+++ app/celery_app/tasks/process_file.py
@@ -1,2 +1,5 @@
 from celery_app.celery import app
 
+#retry_backoff=celery waits for sometime before retry after failure
+@app.task(bind=True, autoretry_for=(Exception,),retry_backoff=True, retry_kwargs={"max_retries":3})
+
