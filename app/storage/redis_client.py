753c6ea3c4c2e92f9bdbc1acb97f38d3f59a265f
rashmib21 <rashmibarethiya2130@gmail.com>
2026-07-22 16:45:13 +0530

    create connection with redis server

--- app/storage/redis_client.py
+++ app/storage/redis_client.py
@@ -1,4 +1,13 @@
 #it is mainly responsible for redisearch index that makes semantic (vector) search possible
 import redis
-from redis.commands.search.field import VectorField, TextField #(the paramaters are path, file type, embedding-vector - the first two is text and third one is vector)
 
+from redis.commands.search.field import VectorField, TextField #(the paramaters are path, file type, embedding-vector - the first two is text and third one is vector)
+#redis perform semantic search on vector
+
+from redis.commands.search.indexDefinition import(IndexDefinition, IndexType)
+#only build index for keys beginning with chunks, IndexType is used for storing HASH value in key pair -path, file_type, text, embeddings etc
+
+#Connect to redis
+r=redis.Redis(
+	host=REDIS_HOST,
+	port=REDIS_PORT) #this creates connection with redis server
