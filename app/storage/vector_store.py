102f9c684619cbd9fcfb6459e17b8450bf19019d
rashmib21 <rashmibarethiya2130@gmail.com>
2026-07-22 19:25:11 +0530

    connect to redis host and port

--- app/storage/vector_store.py
+++ app/storage/vector_store.py
@@ -1,3 +1,11 @@
 #this file is created to store embeddings permanently
+
 import redis #Connect to redis database
 import numpy as np #convert the embedding (python list) into binary bytes because redis expects vectors in byte format
+from config import REDIS_HOST, REDIS_PORT
+
+r=redis.Redis(
+	host=REDIS_HOST,
+	port=REDIS_PORT,
+	decode_response=False #normally redis converts bytes into string, but embeddings are binary data, not text
+	)
