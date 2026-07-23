7e46bf97e784051e45a067b1cf79c8820d9d8008
rashmib21 <rashmibarethiya2130@gmail.com>
2026-07-23 16:16:33 +0530

    create document.py to extract text from documents like pdf

---
+++ app/extractors/document.py
@@ -0,0 +1,5 @@
+import pdfplumber #copying text from pdf
+import pytesseract #scan a paper and make pdf, pytesseract performs OCR 
+from pdf2image import convert_from_path #OCR can't work directly on a pdf
+
+
