69cafc1602cf928d5da903b17b06b7dd72213f10
rashmib21 <rashmibarethiya2130@gmail.com>
2026-07-23 23:17:08 +0530

    import docx in document.py

--- app/extractors/document.py
+++ app/extractors/document.py
@@ -1,7 +1,7 @@
 import os
 from app.extractors.pdf import extract_pdf
 from app.extractors.text import extract_txt
-
+from app.extractors.docx import extract_docx
 def extract_text(path):
 	#Detect file extension and call the correct extractor
 	#Split filename and extension
@@ -14,6 +14,9 @@ def extract_text(path):
 
 	if extension==".pdf":
 		return extract_pdf(path)
-	elif extension in [".txt", ".md", ".log", ".json", ".xml"]:
+	elif extension in [".txt", ".log", ".md", ".json", ".xml", ".yaml", ".yml", ".ini", ".cfg", ".conf"]:
 		return extract_txt(path)	
+	elif extension in [".docx", ".doc"]:
+		return extract_docx(path)
+			
 	raise ValueError(f"Unsupported file type: {extension}")	
