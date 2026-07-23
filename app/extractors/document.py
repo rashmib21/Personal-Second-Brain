701fe87d0d9b49535b79a69dab5353d8d0fc807a
rashmib21 <rashmibarethiya2130@gmail.com>
2026-07-23 22:31:39 +0530

    add pdf extension detection

--- app/extractors/document.py
+++ app/extractors/document.py
@@ -1,3 +1,16 @@
 import os
 from app.extractors.pdf import extract_pdf
 
+def extract_text(path):
+	#Detect file extension and call the correct extractor
+	#Split filename and extension
+	_, extension=os.path.splitext(path)
+
+	#convert to lowercase
+	extension=extension.lower()
+
+	print(f"Detected extension: {extension}")
+
+	if extension==".pdf":
+		return extract_pdf(path)
+	raise ValueError(f"Unsupported file type: {extension}")	
