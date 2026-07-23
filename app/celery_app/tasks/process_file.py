272ecc64e7014686ec5f24814a1a51f77f587438
rashmib21 <rashmibarethiya2130@gmail.com>
2026-07-23 18:24:53 +0530

    add auto file_path instead of manual send file

--- app/celery_app/tasks/process_file.py
+++ app/celery_app/tasks/process_file.py
@@ -1,6 +1,11 @@
 from app.celery_app.celery import app
 import logging
 import time
+import os
+
+from app.extractors.document import extract_text
+from app.chunker.chunker import chunk_text
+
 
 logger=logging.getLogger(__name__)
 
@@ -9,10 +14,32 @@ logger=logging.getLogger(__name__)
 
 
 def process_file(self, path):
+
+	file_name=os.path.basename(path)
+	print(f"*****{file_name}*****")
+
 	logger.info(f"Started Processing {path}")
 
+	#Step 1: Extract text from the document
+	text=extract_text(path)
+
+	logger.info("Text extracted successfully")
+
+	#Step 2: Split text into chunks
+	chunks=chunk_text(text)
+
+	logger.info(f"Created {len(chunks)} chunks")
+
+	#print chunks temporarily
+	for i, chunk in enumerate(chunks, start=1):
+		logger.info(f"Chunk {i}:\n{chunk}\n")
+
 	#long running task
-	time.sleep(30)
+	# time.sleep(30)
 	logger.info(f"Finished Processing {path}")
 
-	return {"path":path, "status":"processed"}
+	return {
+		"path":path, 
+		"total_chunks":len(chunks), 
+		"status":"processed"
+	}
