3a22623e7ba125a0a9971d39774ba3eee50889d7
rashmib21 <rashmibarethiya2130@gmail.com>
2026-07-24 12:37:53 +0530

    add image, video, audio function and extensions list

--- app/extractors/document.py
+++ app/extractors/document.py
@@ -3,6 +3,11 @@ from app.extractors.pdf import extract_pdf
 from app.extractors.text import extract_txt
 from app.extractors.docx import extract_docx
 from app.extractors.spreadsheet import extract_spreadsheet
+from app.extractors.image import extract_image
+from app.extractors.audio import extract_audio
+from app.extractors.video import extract_video
+from app.extractors.archive import extract_archive
+
 def extract_text(path):
 	#Detect file extension and call the correct extractor
 	#Split filename and extension
@@ -21,7 +26,15 @@ def extract_text(path):
 		return extract_docx(path)
 	elif extension in [".csv", ".xlsx", ".xls", ".ods"]:
 		return extract_spreadsheet(path)
-		
+	elif extension in [".jpg", ".jpeg", ".png", ".bmp", ".gif", ".tiff", ".tif", ".webp", ".heic"]:
+		return extract_image(path)
+	elif extension in [".mp3", ".wav", ".m4a",".aac", ".flac", ".ogg", ".wma"]:
+		return extract_audio(path)
+	elif extension in [".mp4", ".avi", ".mov", ".mkv", ".wmv", ".flv", ".webm", ".mpeg", ".3gp"]	
+		return extract_video(path)
+	elif extension in [".zip", ".rar", ".7z",".tar", ".gz"]:
+		return extract_archive(path)	
+
 	
 
 	raise ValueError(f"Unsupported file type: {extension}")	
