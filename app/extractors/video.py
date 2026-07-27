4771e875d361e91a7ede9e8f46b90a1aecf9750b
rashmib21 <rashmibarethiya2130@gmail.com>
2026-07-27 15:16:07 +0530

    initialize function of keyframes

--- app/extractors/video.py
+++ app/extractors/video.py
@@ -1,10 +1,17 @@
 import subprocess #-run ffmpeg
-import whisper  #convert speech to text
+# import whisper  #convert speech to text
+import os
+import cv2
 
+from app.extractors.audio import extract_audio
 
-whisper_model=whisper.load_model('base')
+# whisper_model=whisper.load_model('base')
 
-def extract_video(path):
-	with open(path, 'r', encoding='utf-8'):
-		video=file.read()
-	return video	
+def extract_keyframes(path, interval_sec=8):
+	
+	#Open the video
+	video=cv2.VideoCapture(path)
+
+	#Frames per second
+	fps=video.get(cv2.CAP_PROP_FPS)
+	
