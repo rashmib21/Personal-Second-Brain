8daa1002d00e813898a49aa2be1003f8dc47fa88
rashmib21 <rashmibarethiya2130@gmail.com>
2026-07-27 14:45:26 +0530

    audio.py is completed with testing

--- app/extractors/audio.py
+++ app/extractors/audio.py
@@ -1,4 +1,23 @@
+import whisper
+
+#Load the whisper model once when this module is imported
+whisper_model=whisper.load_model("base")
+
 def extract_audio(path):
-	with open(path, 'r', encoding='utf-8'):
-		audio=file.read()
-	return audio	
+	
+	#Transcribe the audio
+	result=whisper_model.transcribe(path, fp16=False,
+    verbose=True)
+	print(result["language"])
+	print(result["text"])
+
+
+
+	#Return only the text
+	return result['text']
+
+#Testing
+if __name__=='__main__':
+	file_path='watched_folder/MLKDream.mp3'
+	text=extract_audio(file_path)
+	print(text)		
