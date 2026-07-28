107621a5b66c5c1c4c4aab1e592326cd9a897770
rashmib21 <rashmibarethiya2130@gmail.com>
2026-07-28 15:31:34 +0530

    Initialize function for API response in dictionary form

---
+++ app/services/speech_to_text.py
@@ -0,0 +1,41 @@
+import os
+from elevenlabs.client import ElevenLabs
+from config import ELEVENLABS_API_KEY
+
+#Create the client once when the module is imported
+client=ElevenLabs(
+	api_key=ELEVENLABS_API_KEY
+	)
+
+def transcribe_audio(audio_path):
+	#Transcribe an audio file using elevenlabs scribe
+	try:
+		with open(audio_path,'rb') as audio_file:
+			response=client.speech_to_text.convert(
+				file=audio_file,
+				model_id="scribe_v2",
+				diarize=True,
+				tag_audio_events=True,
+				#None =auto language detection
+				language_code=None
+				)
+		return {
+			"text":response.text,
+			"language":response.language_code,
+			"words":response.words,
+		}	
+	except Exception as e:
+		raise RuntimeError(f"Speech-to-text failed: {e}")	
+
+
+#Testing
+if __name__ == "__main__":
+
+    file_path = "watched_folder/MLKDream.mp3"
+
+    result = transcribe_audio(file_path)
+
+    print("=" * 70)
+    print("Language :", result["language"])
+    print("=" * 70)
+    print(result["text"])
