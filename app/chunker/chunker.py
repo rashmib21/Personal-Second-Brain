8c47c5e55d6039523a8a7dab225e80340e49e64c
rashmib21 <rashmibarethiya2130@gmail.com>
2026-07-28 16:41:36 +0530

    add condition if there is no text return empty list

--- app/chunker/chunker.py
+++ app/chunker/chunker.py
@@ -1,7 +1,13 @@
 def chunk_text(text, chunk_size=500):
+
+	#Return empty list if there is no text
+	if not text:
+		return []
+
 	#split text into chunks of fixed size
 	chunks=[]
+
 	for i in range(0, len(text), chunk_size):
 		chunk=text[i:i + chunk_size]
-		chunks.append(chunk)
+		chunks.append(chunk.strip())
 	return chunks	
