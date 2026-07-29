6b89a3f48a1e1071728391d302bd45876c08c7d8
rashmib21 <rashmibarethiya2130@gmail.com>
2026-07-29 16:06:18 +0530

    initialize function of image embedding

--- app/embeddings/image_embedding.py
+++ app/embeddings/image_embedding.py
@@ -1,2 +1,18 @@
 from PIL import Image
-from app.extractors.image import clip_model
+from sentence_transformers import SentenceTransformer
+
+#Load clip model
+clip_model = SentenceTransformer("clip-ViT-B-32")
+
+def embed_image(image_path):
+	#Generate an ebmbedding for an image
+	image=Image.open(image_path).convert("RGB")
+	embedding=clip_model.encode(image)
+	return embedding.tolist()
+
+#Testing
+if __name__=="__main__":
+	image_path="watched_folder/passport_size.png"
+	embedding=embed_image(image_path)
+	print(f"Embedding Dimension: {len(embedding)}")
+	print(embedding)
