0b395d9f4b406469024604addb10d743d0e58d39
rashmib21 <rashmibarethiya2130@gmail.com>
2026-07-29 16:12:33 +0530

    initialize function for generate embeddings according to file type

--- app/embeddings/embedding_router.py
+++ app/embeddings/embedding_router.py
@@ -1,2 +1,16 @@
 from app.embeddings.text_embedding import embed_text
 from app.embeddings.image_embedding import embed_image
+
+def generate_embedding(file_type, content):
+	#Generate embedding based on file_type
+
+	#Text based
+	if file_type in (
+		"pdf","document","text","audio","video_transcript",):
+		return embed_text(content)
+
+	#Image based 
+	elif file_type in ("image","video_frame",):
+		return embed_image(content)
+
+	raise ValueError(f"Unspported file type: {file_type}")	
