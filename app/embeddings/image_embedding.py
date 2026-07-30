333107f4a2e46097d61f4ebcf46858d44ad3193b
rashmib21 <rashmibarethiya2130@gmail.com>
2026-07-30 18:46:09 +0530

    comment out main method

--- app/embeddings/image_embedding.py
+++ app/embeddings/image_embedding.py
@@ -10,9 +10,9 @@ def embed_image(image_path):
 	embedding=clip_model.encode(image)
 	return embedding.tolist()
 
-#Testing
-if __name__=="__main__":
-	image_path="watched_folder/passport_size.png"
-	embedding=embed_image(image_path)
-	print(f"Embedding Dimension: {len(embedding)}")
-	print(embedding)
+# #Testing
+# if __name__=="__main__":
+# 	image_path="watched_folder/passport_size.png"
+# 	embedding=embed_image(image_path)
+# 	print(f"Embedding Dimension: {len(embedding)}")
+# 	print(embedding)
