3afa58b9e6577048e748b3a737adb888a33e4fda
rashmib21 <rashmibarethiya2130@gmail.com>
2026-07-30 22:56:20 +0530

    correct the function of text

--- app/extractors/text.py
+++ app/extractors/text.py
@@ -1,8 +1,9 @@
 def extract_txt(path):
+    """
+    Extract text from a plain text file.
+    """
 
-	#extract text from a plain text .txt file
-	#open the file
-	with open(path,'r', encoding='utf-8') as file:
-		#Read the complete file
-		text=file.read()
-	return text	
+    with open(path, "r", encoding="utf-8") as file:
+        text = file.read()
+
+    return text
