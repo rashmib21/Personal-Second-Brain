c4c4f92dd106b88e3558f17e0ffed9494e30f4f5
rashmib21 <rashmibarethiya2130@gmail.com>
2026-07-30 23:04:34 +0530

    correct the issues

--- app/watcher/observer.py
+++ app/watcher/observer.py
@@ -1,1 +1,22 @@
 #Responsible for Creating the Watchdog observer, starting it, stopping it  
+
+import time
+from watchdog.observers import Observer
+from config import WATCHED_FOLDER
+from app.watcher.handler import SecondBrainHandler
+
+observer=Observer()
+handler=SecondBrainHandler()
+
+observer.schedule(handler, WATCHED_FOLDER, recursive=True)
+observer.start()
+
+print(f"Watching: {WATCHED_FOLDER}")
+
+try: 
+	while True:
+		time.sleep(1)
+except KeyboardInterrupt:
+	observer.stop()
+
+observer.join()			
