2a58092a69d6202ebe95be57b7d022235d7f5491
rashmib21 <rashmibarethiya2130@gmail.com>
2026-07-30 23:05:29 +0530

    move folder into app

---
+++ app/utils/logger.py
@@ -0,0 +1,30 @@
+import logging
+import sys #use to print logs in terminals
+
+#create a logger object
+logger=logging.getLogger()
+
+#show info, warning, error, and critical msg
+logger.setLevel(logging.INFO)
+
+#print all logs in the terminals
+handler=logging.StreamHandler(sys.stdout)
+
+#set the format of logs in which pattern they will be print
+formatter=logging.Formatter(
+	"%(asctime)s | %(levelname)s | %(message)s")
+#asctime: time, levelname: info, error, warning, message: log message like Scanner started
+
+#attach the formatter to the handler, every log printed by this handler 
+handler.setFormatter(formatter)
+
+#attach the handler to the logger, logger knows where to send logs and how to format them
+logger.addHandler(handler)
+
+#use the logger
+logger.info("Scanner started...")
+logger.warning("Folder is empty...")
+logger.error("Redis connection failed.. try again...")
+
+
+
