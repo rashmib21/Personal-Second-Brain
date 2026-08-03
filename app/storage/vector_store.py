0f5a4cf34570d8d75935e101d3ddc046ce16446c
rashmib21 <rashmibarethiya2130@gmail.com>
2026-08-03 15:33:14 +0530

    create schema of table

---
+++ app/storage/vector_store.py
@@ -0,0 +1,20 @@
+import lancedb
+import pyarrow as pa
+
+db=lancedb.connect("lancedb_data")
+
+schema=pa.schema([
+	("id",pa.string()),
+	("path",pa.string()),
+	("file_type",pa.string()),
+	("text",pa.string()),
+	("embedding",pa.list_(pa.float32(),384))
+])
+
+table=db.create_table(
+	"documents",
+	schema=schema,
+	mode="overwrite"
+)
+
+print(table)
