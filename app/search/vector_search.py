153062ca37c5ed954233f0c3f46395e468603506
rashmib21 <rashmibarethiya2130@gmail.com>
2026-08-03 15:31:12 +0530

    remove the unnecessary thing

--- app/search/vector_search.py
+++ app/search/vector_search.py
@@ -1,9 +1,7 @@
-import numpy as np
-from redis.commands.search.query import Query
 
-from app.storage.redis_store import r
+from app.storage.lancedb_store import table
 from app.embeddings.embedding_router import generate_embedding
-
+	
 MAX_ALLOWED_DISTANCE = 0.5
 
 def search(question, how_many_results=5):
@@ -13,52 +11,13 @@ def search(question, how_many_results=5):
 		question
 	)
 
-	#Convert embedding list to bytes
-	query_vector_bytes=np.array(
-		question_vector,
-		dtype=np.float32
-	).tobytes()
-
-	#KNN Vector Search
-	"""
-		* means search every document, => perform vector operation,
-		KNNtop-K return relevant closes vector,
-		@embeddings means field field contain vector, our index has embedding VECTOR, compare with this field
-		$vector use the embedding of the user's question
-
-	"""
-	#Build the KNN search
-	search_command=("*=>"
-		f"[KNN {how_many_results} @embeddings $vector AS distance_score]")
-
-	#create query object
-	query=Query(search_command)
-
-	#Sort by similarity score
-	query=query.sort_by("distance_score")
-
-	#Return only these fields from Redis
-	query=query.return_fields(
-		"path",
-		"file_type",
-		"text",
-		"distance_score"
-	)
-
-	#Enable Redisearch query dialect 2->it is used to enable vector search in KNN
-	query=query.dialect(2)
-
-	#Execute the search
-	search_results=r.ft("idx:files").search(
-		query,
-		{
-			"vector":query_vector_bytes
-		}
-	)
+	results=(table.search(question_vector).metric("cosine").limit(how_many_results).to_list())
+	
 	#Only keep results that are close enough to be real matches
 	good_results=[]
-	for one_result in search_results.docs:
-		distance=float(one_result.distance_score)
+	for one_result in results:
+
+		distance=float(one_result["_distance"])
 		if distance<=MAX_ALLOWED_DISTANCE:
 			good_results.append(one_result)
 
@@ -75,9 +34,9 @@ if __name__=="__main__":
 
 	for position, chunk in enumerate(matching_chunks, start=1):
 		print("="*100)
-		print(f"Result {i}")
-		print("Path: ",chunk.path)
-		print("Type: ",chunk.file_type)
-		print("Score: ",chunk.score)
+		print(f"Result {position}")
+		print("Path: ",chunk["path"])
+		print("Type: ",chunk["file_type"])
+		print("Score: ",chunk["_distance"])
 		print("Text: ")
-		print(chunk.text)	
+		print(chunk["text"])	
