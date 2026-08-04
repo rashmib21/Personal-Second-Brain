c3098c9aec08f0fa6ce0b417c674da34f4779784
rashmib21 <rashmibarethiya2130@gmail.com>
2026-08-04 14:23:11 +0530

    add debug code

--- app/kafka_producer/producer.py
+++ app/kafka_producer/producer.py
@@ -1,35 +1,46 @@
 from kafka import KafkaProducer
 import json
+import time
+
 from config import KAFKA_BOOTSTRAP_SERVERS
 
 
 def value_serializer(v):
-	return json.dumps(v).encode('utf-8')
+    return json.dumps(v).encode("utf-8")
+
 
 def key_serializer(v):
-	return json.dumps(v).encode('utf-8')
+    return str(v).encode("utf-8")
 
 
-producer=KafkaProducer(
-	bootstrap_servers=KAFKA_BOOTSTRAP_SERVERS,
-	value_serializer=value_serializer,
-	key_serializer=key_serializer
-	)	
+producer = KafkaProducer(
+    bootstrap_servers=KAFKA_BOOTSTRAP_SERVERS,
+    value_serializer=value_serializer,
+    key_serializer=key_serializer,
+)
 
-def publish_file_event(event_type, path):
-	#create event data
-	event={
-	"event":event_type,
-	"path":path,
-	"timestamp":time.time()
-	}
 
-	#Send event to kafka producer
-	producer.send(
-		"file-events", #kafka topic
-		key=path,
-		value=event
-		)
+def publish_file_event(event):
+    # Add timestamp
+    event["timestamp"] = time.time()
 
-	#check message is send
-	producer.flush()
+    try:
+        future = producer.send(
+            "file-events",
+            key=event["path"],
+            value=event,
+        )
+
+        metadata = future.get(timeout=10)
+
+        print(
+            f"Sent successfully -> "
+            f"Topic: {metadata.topic}, "
+            f"Partition: {metadata.partition}, "
+            f"Offset: {metadata.offset}"
+        )
+
+    except Exception as e:
+        print(f"Producer Error: {e}")
+
+    producer.flush()
