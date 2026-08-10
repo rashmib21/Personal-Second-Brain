c06e2fb6e8e82176dbdf7507427dcc92754432b0
rashmib21 <rashmibarethiya2130@gmail.com>
2026-08-10 15:35:18 +0530

    correct the issues

--- app/utils/hash.py
+++ app/utils/hash.py
@@ -3,11 +3,14 @@ import hashlib
 
 def calculate_file_hash(path):
 
+    #object of sha256
     sha256 = hashlib.sha256()
 
     with open(path, "rb") as file:
+        chunk=file.read(8192) #read 8kb at a time
 
-        while chunk := file.read(8192):
+        while chunk:  
             sha256.update(chunk)
+            chunk=file.read(8192)
 
     return sha256.hexdigest()
