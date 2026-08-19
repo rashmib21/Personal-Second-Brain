ea2cdbe56cfa07907f9b29419fa916662c325be8
rashmib21 <rashmibarethiya2130@gmail.com>
2026-08-19 21:29:49 +0530

    create table for image embedding store

--- app/storage/lancedb_store.py
+++ app/storage/lancedb_store.py
@@ -9,6 +9,8 @@ DB_PATH = os.path.join(BASE_DIR, "database")
 
 TABLE_NAME="documents"
 HASH_TABLE_NAME="processed_files"
+IMAGE_TABLE_NAME = "image_documents"
+
 
 #create database folder if it doesn't exists
 os.makedirs(DB_PATH,exist_ok=True)
@@ -16,7 +18,7 @@ os.makedirs(DB_PATH,exist_ok=True)
 #Connect to lancedb
 db=lancedb.connect(DB_PATH)
 
-
+#-------Document Table--------
 #create table
 def get_table():
 	tables = db.list_tables().tables
@@ -29,14 +31,39 @@ def get_table():
 		pa.field("path", pa.string()),
 		pa.field("file_type", pa.string()),
 		pa.field("text", pa.string()),
+		
+		#Text embedding
 		pa.field("embedding", pa.list_(pa.float32(),384)),
 		])
+
 
 	return db.create_table(
 		TABLE_NAME, schema=schema)
 
-		
 
+#-----Image Document Table------
+def get_image_table():
+	if IMAGE_TABLE_NAME in db.list_tables().tables:
+		return db.open_table(IMAGE_TABLE_NAME)
+
+	schema=pa.schema([
+			pa.field("chunk_id",pa.string()),
+			pa.field("path", pa.string()),
+			pa.field("file_type", pa.string()),
+			pa.field("text",pa.string()),
+
+			#CLIP image embedding=512 dimensions
+			pa.field("image_embedding", pa.list_(pa.float32(), 512)
+				),
+			])
+	return db.create_table(
+		IMAGE_TABLE_NAME,
+		schema=schema
+	)					
+
+
+
+#Processed file hash table
 def get_hash_table():
 	if HASH_TABLE_NAME in db.list_tables().tables:
 		return db.open_table(HASH_TABLE_NAME)
@@ -50,7 +77,11 @@ def get_hash_table():
 
 table=get_table()
 hash_table=get_hash_table()
+image_table = get_image_table()
 
+
+
+#File hash function
 def is_file_processed(file_hash):
 	result=hash_table.search().where(f"file_hash='{file_hash}'").limit(1).to_list()
 	return len(result) > 0
@@ -67,19 +98,35 @@ def save_file_hash(file_hash, path):
 
 #Store one chunk
 def store_chunk(chunk_id,path, file_type, text, embedding):
-	#Store one document chunk into LanceDB
+	#Store one document chunk into LanceDB.
+	#	
+	
 	table.add([
 		{
 			"chunk_id":chunk_id,
 			"path":path,
 			"file_type":file_type,
 			"text":text,
-			"embedding":embedding
-
+			"embedding":embedding,
 		}
 	])
 
 	print(f"Stored Chunk: {chunk_id}")
+
+#Image chunk store
+def store_image_chunk(chunk_id, path, file_type, text, image_embedding):
+	#Store image information and its 512-D CLIP embedding in the separate image_documents table.
+	image_table.add([
+		{
+			"chunk_id":chunk_id,
+			"path":path,
+			"file_type":file_type,
+			"text":text,
+			"image_embedding":image_embedding,
+		}
+	])	
+
+	print(f"Stored Image Chunks: {chunk_id}")
 
 #Show all records
 def show_all():
