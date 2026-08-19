ce144de3027a074aa87a5fe1ef88024497568254
rashmib21 <rashmibarethiya2130@gmail.com>
2026-08-19 16:05:24 +0530

    add condition for image embedding

--- app/embeddings/embedding_router.py
+++ app/embeddings/embedding_router.py
@@ -1,10 +1,16 @@
 from app.embeddings.text_embedding import embed_text
+from app.embeddings.image_embedding import embed_image
 
 def generate_embedding(file_type, content):
     """
     Generates a 384-dimensional text embedding for all text chunks
     (PDF, DOCX, TXT, OCR text, ASR transcripts, Archives, Presentations).
     """
+    #Image embeddings
+    if file_type=="image":
+        return embed_image(content)
+
+    #Text embeddings for all other file types
     if not isinstance(content, str):
         content = str(content)
 
