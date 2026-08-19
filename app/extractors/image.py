dd41713c80f6ff3a96ba0aadc5e970fe2c0b638e
rashmib21 <rashmibarethiya2130@gmail.com>
2026-08-19 15:59:11 +0530

    declare a variable outside the condition

--- app/extractors/image.py
+++ app/extractors/image.py
@@ -20,6 +20,7 @@ def extract_image(path):
         #Convert image to OpenCV format
         image_cv=cv2.imread(path)
         face_count=0
+        faces_data=[]
 
         if image_cv is not None:
             gray=cv2.cvtColor(image_cv, cv2.COLOR_BGR2GRAY)
@@ -34,6 +35,14 @@ def extract_image(path):
                 minSize=(30,30)
                 )
             face_count=len(faces)
+
+            #Store information about each detected face
+            
+            for index, (x,y,w,h) in enumerate(faces):
+                faces_data.append({
+                        "face_id":index,
+                        "bbox":[int(x), int(y), int(w), int(h)]
+                    })
 
         #Build searchable text
         extracted_parts=[]
@@ -50,7 +59,8 @@ def extract_image(path):
             "metadata":{
                     "file_type":"image",
                     "ocr_text":text,
-                    "face_count":face_count
+                    "face_count":face_count,
+                    "faces":faces_data
             }
         }        
 
